625afd5c4c59874fce461f936941734551883550
Naoki Kitamura <108145660+nkitamuraQC@users.noreply.github.com>
2024-10-26 00:09:44 +0900

    Create make_docker.sh

---
+++ make_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# コンテナ名（任意で変更可能）
+CONTAINER_NAME="ubuntu_container"
+
+# Dockerコンテナを作成し、バックグラウンドで実行
+docker run -d --name $CONTAINER_NAME ubuntu:latest tail -f /dev/null
+
+# コンテナの中に入る
+docker exec -it $CONTAINER_NAME /bin/bash
